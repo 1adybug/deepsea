e119548f8f0dfb076ca23cb60a202f54f3da1ddc
陆子虚 <11374106+zixulu@user.noreply.gitee.com>
2024-06-18 19:03:46 +0800

    chore: Update deepsea-components-demo version to 0.0.2 and dependencies

--- packages/deepsea-components-demo/CHANGELOG.md
+++ packages/deepsea-components-demo/CHANGELOG.md
@@ -1,5 +1,12 @@
 # deepsea-components-demo
 
+## 0.0.2
+
+### Patch Changes
+
+-   Updated dependencies
+    -   deepsea-components@5.1.1
+
 ## 0.0.1
 
 ### Patch Changes
--- packages/deepsea-components-demo/src/App.tsx
+++ packages/deepsea-components-demo/src/App.tsx
@@ -3,8 +3,8 @@ import { FC } from "react"
 
 const App: FC = () => {
     return (
-        <InfiniteScroll style={{ width: 400, height: 400 }} direction="horizontal" gap={0} duration={10000} withEqual>
-            <img style={{ display: "block" }} src="/a.jpg" width={400} height={400} />
+        <InfiniteScroll style={{ width: 400, height: 400 }} direction="horizontal" gap={0} duration={10000} withEqual pauseOnHover>
+            <div className="w-[400px] h-[400px] bg-blue-300">1</div>
         </InfiniteScroll>
     )
 }
