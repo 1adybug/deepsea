b52493d34715936b847949609b11b4a60e0aed09
陆子虚 <lurongv@qq.com>
2024-07-15 10:55:21 +0800

    🐞 fix: 去除无用的方法，修复 execAsync 乱码问题

--- packages/soda-nodejs/src/utils/execAsync.ts
+++ packages/soda-nodejs/src/utils/execAsync.ts
@@ -1,25 +1,33 @@
 import { exec, ExecOptions } from "child_process"
 import { ObjectEncodingOptions } from "fs"
+import iconv, { Options } from "iconv-lite"
 
-export type ExecResult<T> = {
-    stdout: T
-    stderr: T
+export type IconvDecodeOptions = {
+    encoding: string
+    options?: Options
 }
 
-export async function execAsync(command: string): Promise<ExecResult<string>>
-export async function execAsync(command: string, options: { encoding: "buffer" | null } & ExecOptions): Promise<ExecResult<Buffer>>
-export async function execAsync(command: string, options: { encoding: BufferEncoding } & ExecOptions): Promise<ExecResult<string>>
-export async function execAsync(command: string, options: { encoding: BufferEncoding } & ExecOptions): Promise<ExecResult<string | Buffer>>
-export async function execAsync(command: string, options: ExecOptions): Promise<ExecResult<string>>
-export async function execAsync(command: string, options: (ObjectEncodingOptions & ExecOptions) | undefined | null): Promise<ExecResult<string | Buffer>>
+export async function execAsync(command: string): Promise<string>
+export async function execAsync(command: string, options: { encoding: "buffer" | null } & ExecOptions): Promise<Buffer>
+export async function execAsync(command: string, options: { encoding: "buffer" | null } & ExecOptions & { decode: IconvDecodeOptions }): Promise<string>
+export async function execAsync(command: string, options: { encoding: BufferEncoding } & ExecOptions): Promise<string>
+export async function execAsync(command: string, options: { encoding: BufferEncoding } & ExecOptions): Promise<string | Buffer>
+export async function execAsync(command: string, options: { encoding: BufferEncoding } & ExecOptions & { decode: IconvDecodeOptions }): Promise<string>
+export async function execAsync(command: string, options: ExecOptions): Promise<string>
+export async function execAsync(command: string, options: (ObjectEncodingOptions & ExecOptions) | undefined | null): Promise<string | Buffer>
+export async function execAsync(command: string, options: (ObjectEncodingOptions & ExecOptions & { decode: IconvDecodeOptions }) | undefined | null): Promise<string>
 export async function execAsync(command: string, options?: any) {
-    return await new Promise<ExecResult<string | Buffer>>((resolve, reject) => {
+    const decode = options?.decode as IconvDecodeOptions | undefined
+    if (typeof options === "object" && options !== null && options.decode) {
+        const { decode, ...rest } = options as { decode: IconvDecodeOptions }
+        options = rest
+    }
+    return await new Promise<string | Buffer>((resolve, reject) => {
         exec(command, options, (error, stdout, stderr) => {
-            if (error) {
-                reject(error)
-                return
-            }
-            resolve({ stdout, stderr })
+            if (error) return reject(error)
+            // if (stderr) console.warn(stderr)
+            if (decode && stdout instanceof Buffer) return resolve(iconv.decode(stdout, decode.encoding, decode.options))
+            resolve(stdout)
         })
     })
 }
