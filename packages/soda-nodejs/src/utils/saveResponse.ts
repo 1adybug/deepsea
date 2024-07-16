30a10e3fb33d942a93ea04630b0ecccfc7d25de5
陆子虚 <lurongv@qq.com>
2024-07-16 18:42:05 +0800

    ✨feature: 新增 saveResponse 方法

---
+++ packages/soda-nodejs/src/utils/saveResponse.ts
@@ -0,0 +1,17 @@
+import { createWriteStream } from "fs"
+import { Readable } from "stream"
+
+/**
+ * 从 Response 对象中下载文件
+ * @param response Response 对象
+ * @param file 文件路径
+ */
+export async function saveResponse(response: Response, file: string) {
+    const writeable = createWriteStream(file)
+    await new Promise((resolve, reject) =>
+        Readable.fromWeb(response.body! as any)
+            .pipe(writeable)
+            .on("close", resolve)
+            .on("error", reject)
+    )
+}
