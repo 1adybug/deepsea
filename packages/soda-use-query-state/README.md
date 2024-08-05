18d259ecf20cdb47817a3aa1df52e1d7e039a5d9
陆子虚 <11374106+zixulu@user.noreply.gitee.com>
2024-08-05 18:29:43 +0800

    ✨feature: 新增 soda-use-query-state

---
+++ packages/soda-use-query-state/README.md
@@ -0,0 +1,23 @@
+# soda-next
+
+[![NPM version](https://img.shields.io/npm/v/soda-next.svg?style=flat)](https://npmjs.com/package/soda-next)
+[![NPM downloads](http://img.shields.io/npm/dm/soda-next.svg?style=flat)](https://npmjs.com/package/soda-next)
+
+## Install
+
+```bash
+$ pnpm install
+```
+
+```bash
+$ npm run dev
+$ npm run build
+```
+
+## Options
+
+TODO
+
+## LICENSE
+
+MIT
--- scripts/dev.ts
+++ scripts/dev.ts
@@ -1,16 +1,29 @@
-import { spawn } from "node:child_process"
+import { spawnAsync } from "soda-nodejs"
 import { readdir, readFile } from "node:fs/promises"
 
 async function main() {
     const dir = await readdir("packages")
+    const dependencies: Record<string, string[]> = {}
     for (const item of dir) {
         const packageJSON = JSON.parse(await readFile(`packages/${item}/package.json`, "utf-8"))
-        if (!packageJSON.devDependencies?.father) return
-        spawn(`pnpm dev`, {
-            cwd: `packages/${item}`,
-            shell: true,
-            stdio: "inherit"
+        if (packageJSON.private) continue
+        dependencies[item] = Object.entries({
+            ...packageJSON.dependencies,
+            ...packageJSON.devDependencies,
+            ...packageJSON.peerDependencies,
+            ...packageJSON.optionalDependencies
         })
+            .filter(([name, version]) => (version as string).includes("workspace:"))
+            .map(([name]) => name)
+    }
+    while (true) {
+        const buildable = Object.keys(dependencies).filter(name => dependencies[name].length === 0)
+        if (buildable.length === 0) break
+        for (const [name] of buildable) {
+            await spawnAsync("pnpm build", { cwd: `packages/${name}`, stdio: "inherit", shell: true })
+            delete dependencies[name]
+            Object.keys(dependencies).forEach(pkg => (dependencies[pkg] = dependencies[pkg].filter(dep => dep !== name)))
+        }
     }
 }
 
