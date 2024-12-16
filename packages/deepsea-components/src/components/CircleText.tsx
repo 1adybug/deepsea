ecf1170582a90e2b21f13fc3c684188887a36949
陆子虚 <11374106+zixulu@user.noreply.gitee.com>
2024-12-16 15:53:34 +0800

    🐞 fix: 修复 FormLabel 组件未导出

--- packages/deepsea-components/src/components/CircleText.tsx
+++ packages/deepsea-components/src/components/CircleText.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { FC, Fragment, HTMLAttributes } from "react"
 
 export interface CircleTextProps extends Omit<HTMLAttributes<HTMLSpanElement>, "children"> {
--- packages/deepsea-components/src/components/Trapezium.tsx
+++ packages/deepsea-components/src/components/Trapezium.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { clsx } from "deepsea-tools"
 import { HTMLAttributes, forwardRef } from "react"
 
