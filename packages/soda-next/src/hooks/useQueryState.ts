a55a13e6c277d2c652bc6edd250e430c5103be22
陆子虚 <11374106+zixulu@user.noreply.gitee.com>
2024-12-16 14:28:37 +0800

    ✨feature: 新增替换选项

--- packages/soda-next/src/hooks/useQueryState.ts
+++ packages/soda-next/src/hooks/useQueryState.ts
@@ -1,20 +1,30 @@
 import { usePathname, useRouter, useSearchParams } from "next/navigation"
+import { SetStateAction } from "react"
 import { QueryState, QueryStateOptions, QueryToStateFnMap, SetQueryState, useNativeQueryState } from "soda-hooks"
+
+export type SetQueryStateOptions = {
+    /** 是否替换页面 */
+    replace?: boolean
+    /** 是否滚动到页面顶部 */
+    scroll?: boolean
+}
 
 /**
  * 使用 Next 的 useSearchParams 实现的 useQueryState
  */
 export function useQueryState<T extends string = never, K extends QueryToStateFnMap = QueryToStateFnMap>(
-    options?: QueryStateOptions<T, K>,
+    options?: QueryStateOptions<T, K> & SetQueryStateOptions,
 ): [QueryState<T, K>, SetQueryState<T, K>] {
+    const { replace, scroll = false, ...rest } = options ?? {}
     const router = useRouter()
     const pathname = usePathname()
     const searchParams = useSearchParams()
-    function setSearchParams(next: URLSearchParams | ((prev: URLSearchParams) => URLSearchParams)) {
-        const newSearchParams = typeof next === "function" ? next(searchParams) : next
+    function setSearchParams(action: SetStateAction<URLSearchParams>) {
+        const newSearchParams = typeof action === "function" ? action(searchParams) : action
         const newSearch = newSearchParams.toString()
-        if (newSearchParams.size === 0) return router.push(pathname)
-        router.push(`${pathname}?${newSearch}`)
+        const newPathname = newSearchParams.size === 0 ? pathname : `${pathname}?${newSearch}`
+        if (replace) router.replace(newPathname, { scroll })
+        else router.push(newPathname, { scroll })
     }
-    return useNativeQueryState({ ...options, search: searchParams, setSearch: setSearchParams })
+    return useNativeQueryState({ ...rest, search: searchParams, setSearch: setSearchParams })
 }
--- packages/soda-react-router/src/hooks/useQueryState.ts
+++ packages/soda-react-router/src/hooks/useQueryState.ts
@@ -1,12 +1,27 @@
+import { SetStateAction } from "react"
 import { useSearchParams } from "react-router-dom"
 import { QueryState, QueryStateOptions, QueryToStateFnMap, SetQueryState, useNativeQueryState } from "soda-hooks"
+
+export type SetQueryStateOptions = {
+    /** 是否替换页面 */
+    replace?: boolean
+    /** 是否滚动到页面顶部 */
+    scroll?: boolean
+}
 
 /**
  * 使用 React Router 的 useSearchParams 实现的 useQueryState
  */
 export function useQueryState<T extends string = never, K extends QueryToStateFnMap = QueryToStateFnMap>(
-    options?: QueryStateOptions<T, K>,
+    options?: QueryStateOptions<T, K> & SetQueryStateOptions,
 ): [QueryState<T, K>, SetQueryState<T, K>] {
+    const { replace, scroll = false, ...rest } = options ?? {}
     const [searchParams, setSearchParams] = useSearchParams()
-    return useNativeQueryState({ ...options, search: searchParams, setSearch: setSearchParams })
+    function setSearch(action: SetStateAction<URLSearchParams>) {
+        setSearchParams(action, {
+            replace,
+            preventScrollReset: !scroll,
+        })
+    }
+    return useNativeQueryState({ ...rest, search: searchParams, setSearch })
 }
