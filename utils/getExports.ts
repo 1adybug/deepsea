f381eb00c8516242ad716a38594f1d44f6537b7e
陆子虚 <lurongv@qq.com>
2025-03-17 11:14:36 +0800

    🐞 fix: 修复导出方式

--- utils/getExports.ts
+++ utils/getExports.ts
@@ -14,11 +14,16 @@ export async function getExports() {
             if (!status2.isFile()) continue
             if (!file.endsWith(".ts") && !file.endsWith(".tsx")) continue
             const content = await readFile(join(folder, file), "utf-8")
-            const variables = Array.from(content.matchAll(/^export (const|let|var|function|class|type|interface) (\w+)/gm))
+            const variables = Array.from(content.matchAll(/^export (const|let|var|function|class) (\w+)/gm))
                 .map(v => v[2])
                 .filter((item, index, array) => array.indexOf(item) === index)
                 .toSorted()
             if (variables.length > 0) exports.push(`export { ${variables.join(", ")} } from "@/${item}/${file.replace(/\.tsx?$/, "")}"`)
+            const types = Array.from(content.matchAll(/^export (type|interface) (\w+)/gm))
+                .map(v => v[2])
+                .filter((item, index, array) => array.indexOf(item) === index)
+                .toSorted().filter(item => !variables.includes(item))
+            if (types.length > 0) exports.push(`export type { ${types.join(", ")} } from "@/${item}/${file.replace(/\.tsx?$/, "")}"`)
         }
     }
     return exports
