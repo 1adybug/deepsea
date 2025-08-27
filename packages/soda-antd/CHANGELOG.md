9cf5655d77e33cf24d2b3d24d189b47f7c3f9df4
陆子虚 <lurongv@qq.com>
2025-08-27 10:51:27 +0800

    ✨feature: 优化 FormSubscribe 参数

--- packages/soda-antd/CHANGELOG.md
+++ packages/soda-antd/CHANGELOG.md
@@ -1,5 +1,11 @@
 # soda-antd
 
+## 0.11.1
+
+### Patch Changes
+
+- 优化 FormSubscribe 参数
+
 ## 0.11.0
 
 ### Minor Changes
--- packages/soda-antd/src/components/FormSubscribe.tsx
+++ packages/soda-antd/src/components/FormSubscribe.tsx
@@ -3,19 +3,21 @@ import { useWatch } from "antd/es/form/Form"
 import FormItem from "antd/es/form/FormItem"
 import useFormInstance from "antd/es/form/hooks/useFormInstance"
 
-export interface FormSubscribeProps<Selector extends (values: any) => any = (values: unknown) => unknown>
-    extends Omit<ComponentProps<typeof FormItem<Parameters<Selector>[0]>>, "children"> {
+export type FormSubscribeProps<Selector extends (values: any) => any = (values: unknown) => unknown, IsItem extends boolean = false> = {
     selector: Selector
+    isItem?: IsItem
     children?: ReactNode | ((data: ReturnType<Selector>) => ReactNode)
-}
+} & (IsItem extends true ? Omit<ComponentProps<typeof FormItem<Parameters<Selector>[0]>>, "children"> : {})
 
-export function FormSubscribe<Selector extends (values: any) => any = (values: unknown) => unknown>({
+export function FormSubscribe<Selector extends (values: any) => any = (values: unknown) => unknown, IsItem extends boolean = false>({
     selector,
+    isItem,
     children,
     ...rest
-}: FormSubscribeProps<Selector>) {
+}: FormSubscribeProps<Selector, IsItem>) {
     const form = useFormInstance<Parameters<Selector>[0]>()
     const data = useWatch(selector, form)
     if (typeof children === "function") children = children(data)
-    return <FormItem {...rest}>{children}</FormItem>
+    if (isItem) return <FormItem {...rest}>{children}</FormItem>
+    return children
 }
