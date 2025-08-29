727c28ab0ee801f14ae3c4a9d4bfccac4644faee
陆子虚 <lurongv@qq.com>
2025-08-29 15:56:34 +0800

    ✨feature: 优化 ESLint 配置

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import globals from "globals"
 import tseslint from "typescript-eslint"
 
 export default tseslint.config([
-    globalIgnores(["dist"]),
+    globalIgnores(["node_modules", "dist", "build", "public"]),
     {
         files: ["**/*.{js,mjs,ts}"],
         extends: [js.configs.recommended, tseslint.configs.recommended],
@@ -16,6 +16,17 @@ export default tseslint.config([
         },
         rules: {
             "@typescript-eslint/no-empty-object-type": "off",
+            "no-empty": "off",
+            "no-extra-boolean-cast": "off",
+            "no-unused-vars": "off",
+            "react-refresh/only-export-components": "warn",
+            "@typescript-eslint/no-unused-vars": [
+                "warn",
+                {
+                    ignoreRestSiblings: true,
+                    args: "none",
+                },
+            ],
             "prefer-const": [
                 "off",
                 {
